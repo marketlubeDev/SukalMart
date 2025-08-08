85c611a477d76e9b5474ef498086a3c3cc0a7661
marketlubes@gmail.com <marketlubedevelopers@gmail.com>
2025-08-08 15:09:55 +0530

    add vercel file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,7 @@
-"use client";
-
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "../layout/user/Nav";
 import Footer from "../layout/user/Footer";
-import { usePathname } from "next/navigation";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -12,18 +9,14 @@ const inter = Inter({
 });
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
-  const isLoginPage = pathname === "/login" || pathname.startsWith("/login/");
-
   return (
     <html lang="en">
       <body
         className={`${inter.variable} antialiased`}
       >
         <Nav />
-       
         {children}
-        {!isLoginPage && <Footer />}
+        <Footer />
       </body>
     </html>
   );
