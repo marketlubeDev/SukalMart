71d09efa7860008c695e9eb759537c90d04ff5f6
marketlubes@gmail.com <marketlubedevelopers@gmail.com>
2025-08-18 17:48:44 +0530

    Rename project from SukalMart to Souqalmart and update package.json description. Remove package-lock.json as part of the cleanup.

---
+++ Sukalmart_admin/src/components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <div>
+      <img
+        src="/public/logo/Logo.png"
+        className="w-3 h-3 md:w-[3rem] md:h-[3rem] object-cover"
+        alt="Logo image"
+      />
+    </div>
+  );
+}
+
+export default Logo;
---
+++ Sukalmart_admin/src/router/router.jsx
@@ -0,0 +1,120 @@
+import { createBrowserRouter } from "react-router-dom";
+import AdminLayout from "../layouts/AdminLayout/AdminLayout";
+import LoginComponent from "../components/shared/LoginComponent";
+import DashBoard from "../pages/admin/DashBoard";
+import Products from "../pages/admin/Products";
+import Orders from "../pages/admin/Orders";
+import Coupons from "../pages/admin/Coupons";
+import Review from "../pages/admin/Review";
+import Customers from "../pages/admin/Customers";
+import Addproduct from "../pages/admin/Addproduct";
+import ProtectedRoute from "./AdminProtectedRoute/ProtectedRoute";
+import Category from "../pages/admin/Category";
+import Brand from "../pages/admin/Brand";
+import Label from "../pages/admin/Label";
+import Banner from "../pages/admin/Banner";
+import BannerWithLink from "../pages/admin/BannerWithLink";
+import Store from "../pages/admin/Store";
+import Storeinfo from "../pages/admin/Storeinfo";
+import StoreLayout from "../layouts/StoreLayout/StoreLayout";
+import Landingpage from "../pages/Landing/Landingpage";
+import { ActiveOffers } from "../pages/admin/ActiveOffers";
+import Sales from "../pages/admin/Sales";
+import Inventory from "../pages/admin/Inventory";
+import StoreProtectedRoute from "./StoreProtectedRoute/StoreProtectedRoute";
+import Subcategories from "../pages/admin/Subcategories";
+import { Feedback } from "../pages/admin/Feedback";
+import Enquiry from "../pages/admin/Enquiry";
+export const router = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   element: <Landingpage />,
+  // },
+  {
+    path: "/",
+    element: <LoginComponent />,
+  },
+  {
+    path: "/admin",
+    element: (
+      <ProtectedRoute>
+        <AdminLayout />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        path: "",
+        element: <DashBoard />,
+      },
+      {
+        path: "product",
+        element: <Products />,
+      },
+      {
+        path: "product/addproduct",
+        element: <Addproduct />,
+      },
+      {
+        path: "order",
+        element: <Orders />,
+      },
+      {
+        path: "category",
+        element: <Category />,
+      },
+      {
+        path: "subcategory",
+        element: <Subcategories />,
+      },
+      {
+        path: "label",
+        element: <Label />,
+      },
+      {
+        path: "coupon",
+        element: <Coupons />,
+      },
+
+      {
+        path: "review",
+        element: <Review />,
+      },
+      {
+        path: "customer",
+        element: <Customers />,
+      },
+      {
+        path: "banner",
+        element: <Banner />,
+      },
+      {
+        path: "banner-with-link",
+        element: <BannerWithLink />,
+      },
+      {
+        path: "active-offer",
+        element: <ActiveOffers />,
+      },
+      // {
+      //   path: "store",
+      //   element: <Store />,
+      // },
+      // {
+      //   path: "storeinfo/:id",
+      //   element: <Storeinfo />,
+      // },
+      {
+        path: "sales",
+        element: <Sales role={"admin"} />,
+      },
+      {
+        path: "inventory",
+        element: <Inventory role={"admin"} />,
+      },
+      {
+        path: "feedback",
+        element: <Feedback />,
+      },
+    ],
+  },
+]);
